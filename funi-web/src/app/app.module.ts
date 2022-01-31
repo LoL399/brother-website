54437cf9ff29aeccdf7a6b19f8b49cfabc7f945c
LoiTo <tvloi3999@gmail.com>
2022-01-31 23:35:24 +0700

    init commit

---
+++ funi-web/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './page/homepage/homepage.component';
+import { IntropageComponent } from './page/intropage/intropage.component';
+import { NavComponentComponent } from './component/nav-componentt/nav-component.component';
+import { FooterComponentComponent } from './component/footer-component/footer-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    IntropageComponent,
+    NavComponentComponent,
+    FooterComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
